7493d05af8b09af9fc5cb1b72422cd8bcb639a4e
desenvolvedorback <desenvolvimentobackend.com@gmail.com>
2025-05-13 23:47:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return 'API rodando com sucesso!'
+    return "API online, chefe!"
+
+if __name__ == "__main__":
+    app.run()
